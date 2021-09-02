a7222667c591a11e98f9e0e1325a131210eb0eab
Daniel Lopez <A00830045@itesm.mx>
2021-09-02 18:58:45 -0500

    entrega 1 condicional basico

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -2,6 +2,19 @@
 def main():
     edad = int(input("Ingresa tu edad: "))
     #Aquí empieza tu programa...
+    if edad < 18 and edad > 0:
+        print("No cumples requisitos")
+    elif edad <= 0:
+            print("Respuesta incorrecta")
+    else:
+        identificacion = str(input("¿Tienes identificación oficial? (s/n): "))
+        
+        if identificacion != "s" and identificacion != "n":
+            print("Respuesta incorrecta")
+        elif edad >= 18 and identificacion == "s":
+            print("Trámite de licencia concedido")
+        else:
+            print("No cumples requisitos")
 
 if __name__ == '__main__':
     main()
--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -3,7 +3,14 @@ def main():
     lado2 = int(input("Ingresa la medida del lado 2: "))
     lado3 = int(input("Ingresa la medida del lado 3: "))
     #Escribe aquí tu código...
-
+    if (lado1 + lado2 < lado3) or (lado1 + lado3 < lado2) or (lado2 + lado3 < lado1):
+        print("NO ES TRIANGULO")
+    elif lado1 == lado2 and lado1 == lado3:
+        print("ES UN TRIANGULO EQUILATERO")
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        print("ES UN TRIANGULO ISOSCELES")
+    else:
+        print("ES UN TRIANGULO ESCALENO")
 
 if __name__=='__main__':
     main()
